caca5d271d8630ad12d72d5d6ebb96b3605fe8ef
Alfonsohr6 <aioyo.hr@gmail.com>
2025-06-14 12:28:50 -0600

    Correccion de carpetas.

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,23 +1,9 @@
 using System;
-using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // Crear listado de formas
-        List<Shape> shapes = new List<Shape>();
-
-        // Agregar cuadrados, rectángulos y círculos
-        shapes.Add(new Square("Red", 5));
-        shapes.Add(new Rectangle("Blue", 6, 4));
-        shapes.Add(new Circle("Green", 3));
-
-        // Mostrar información de todas las formas
-        foreach (var shape in shapes)
-        {
-            Console.WriteLine($"Color: {shape.GetColor()}");
-            Console.WriteLine($"Área: {shape.GetArea()}\n");
-        }
+        Console.WriteLine("Hello World! This is the Shapes Project.");
     }
 }
